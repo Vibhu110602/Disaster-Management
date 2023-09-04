ac564767f54f17f86aa90c86125dae650d92095b
Vaibhav Chachra <vaibhavchachra11@gmail.com>
2023-09-05 00:07:39 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+
+data=pd.read_csv('train.csv')
+x=data.iloc[:,:-1].values
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, len(x)):
+  review = re.sub('[^a-zA-Z]',' ', x[i][3])
+  review = review.lower()
+  review = review.split()
+  ps = PorterStemmer()
+  all_stopwords = stopwords.words('english')
+  all_stopwords.remove('not')
+  review = [ps.stem(word) for word in review if not word in set(all_stopwords)]
+  review = ' '.join(review)
+  corpus.append(review)
+print(corpus[0])
+print(corpus[5])
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv=CountVectorizer(max_features=3000)
+X=cv.fit_transform(corpus).toarray()
+Y=data.iloc[:,-1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.20, random_state = 0)
+
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
